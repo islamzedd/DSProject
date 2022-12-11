b8bd35d14db8665be772f758bcbc4364fb58b07d
unknown <abdelrahmanmohammed13@gmail.com>
2022-12-11 23:53:47 +0200

    added minify function and some edits in chooseXMLFile

--- src/xml/Controller.java
+++ src/xml/Controller.java
@@ -20,9 +20,10 @@ public class Controller {
 	private TextFlow tfOut = new TextFlow();
 	@FXML
 	private TextFlow tfIn = new TextFlow();
-	StringBuilder str_in=new StringBuilder();
-    StringBuilder stringbuilder=new StringBuilder();
-    StringBuilder str=new StringBuilder();
+	private boolean fileExist = false;
+	StringBuilder str_in = new StringBuilder();
+    StringBuilder stringNoTabs = new StringBuilder();
+    StringBuilder tempMinifiedString = new StringBuilder();
 	
 	void init(Stage s) {
         stage=s;
@@ -30,27 +31,37 @@ public class Controller {
 	
 	public void chooseXMLFile(ActionEvent actionevent){
 		FileChooser filechooser = new FileChooser();
-        File file = filechooser.showOpenDialog(stage); //file path
+        File file = filechooser.showOpenDialog(stage); 		//file path
+        
         tfOut.getChildren().clear();
         tfIn.getChildren().clear();
         tfOut.setStyle(" -fx-border-color: Yellow;");
         tfIn.setStyle(" -fx-border-color: Yellow;");
 
-        if(file!=null){
-        	String filename=file.getName();
+        if(file != null){
+        	
+        	fileExist = true;
+        	String filename = file.getName();
            
         	boolean checkxmlfile = filename.contains(".xml")||filename.contains(".txt");
         	if(checkxmlfile){
 
+        		//try catch block is needed for FileReader to work.
         		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
 
         			String line;
+        			
+        			//read each line in the file and stores it in str_in.
+        			//then removes all tabs and empty lines and store it in stringNoTabs to be used later by other functions.
                     while ((line = reader.readLine()) != null) {
                 	    str_in.append(line).append("\n");
                         if(line.isEmpty())
                         	continue;
+                        line = line.replaceAll("  ","");
+                        stringNoTabs.append(line).append("\n");
                     }
         		}
+        		
         	    catch (IOException e) {
         	    	Alert alert = new Alert(AlertType.ERROR);
         	    	alert.setTitle("ERROR");
@@ -59,11 +70,13 @@ public class Controller {
         	    	alert.show();
         	    }
         		
+        		//Creating a Text text to be displayed in the text flow output.
         		Text text = new Text(str_in.toString());
         		tfIn.getChildren().add(text);
         		
         	}
         	
+        	//throw an error if the file is not .xml or .txt
         	else {
         		Alert alert = new Alert(AlertType.ERROR);
         		alert.setTitle("ERROR");
@@ -83,7 +96,36 @@ public class Controller {
 	}
 	 
 	public void minify(ActionEvent action){
-		 
+		
+		//Check if a file has been chosen then clears the text flow output box.
+		if(fileExist){
+			
+			tfOut.getChildren().clear();
+			tfOut.setStyle(" -fx-border-color: Yellow;");			//using a yellow color to highlight usage.
+			StringBuilder minifiedString = new StringBuilder();		
+			
+			//Iterate over the length of stringNoTabs removing all \n in the file to be written in one line. then stores it in minifiedString.
+			for(int i = 0; i < stringNoTabs.length(); i++){
+				if(stringNoTabs.charAt(i) != '\n')
+					minifiedString.append(stringNoTabs.charAt(i));
+			}
+			
+			//Creating a Text t to be displayed in the text flow output.
+			Text t = new Text(minifiedString.toString());
+			tfOut.getChildren().add(t);
+			
+		}
+		
+		//Give an error when there is no file path chosen.
+		else{
+			
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("ERROR");
+			String s ="Please Enter XML File First";
+			alert.setContentText(s);
+			alert.show();
+
+		}
 	}
 	 
 	public void convertJson(ActionEvent a){
