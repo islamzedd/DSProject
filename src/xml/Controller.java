3a5aa3b40fb651a12ddb289d50088d8a59a226aa
ahmed ihab <ahmedehab28001@gmail.com>
2022-12-11 22:09:08 +0200

    added the chooseXMLFile method

--- src/xml/Controller.java
+++ src/xml/Controller.java
@@ -1,44 +1,104 @@
 package xml;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class Controller {
 	private Stage stage;
+	@FXML
+	private TextFlow tfOut = new TextFlow();
+	@FXML
+	private TextFlow tfIn = new TextFlow();
+	StringBuilder str_in=new StringBuilder();
+    StringBuilder stringbuilder=new StringBuilder();
+    StringBuilder str=new StringBuilder();
 	
 	void init(Stage s) {
         stage=s;
     }
 	
-	 public void chooseXMLFile(ActionEvent actionevent){
+	public void chooseXMLFile(ActionEvent actionevent){
+		FileChooser filechooser = new FileChooser();
+        File file = filechooser.showOpenDialog(stage); //file path
+        tfOut.getChildren().clear();
+        tfIn.getChildren().clear();
+        tfOut.setStyle(" -fx-border-color: Yellow;");
+        tfIn.setStyle(" -fx-border-color: Yellow;");
+
+        if(file!=null){
+        	String filename=file.getName();
+           
+        	boolean checkxmlfile = filename.contains(".xml")||filename.contains(".txt");
+        	if(checkxmlfile){
+
+        		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+
+        			String line;
+                    while ((line = reader.readLine()) != null) {
+                	    str_in.append(line).append("\n");
+                        if(line.isEmpty())
+                        	continue;
+                    }
+        		}
+        	    catch (IOException e) {
+        	    	Alert alert = new Alert(AlertType.ERROR);
+        	    	alert.setTitle("ERROR");
+        	    	String s = e.getMessage();
+        	    	alert.setContentText(s);
+        	    	alert.show();
+        	    }
+        		
+        		Text text = new Text(str_in.toString());
+        		tfIn.getChildren().add(text);
+        		
+        	}
+        	
+        	else {
+        		Alert alert = new Alert(AlertType.ERROR);
+        		alert.setTitle("ERROR");
+        		String s ="Please Enter XML File";
+        		alert.setContentText(s);
+                alert.show(); 
+        	}
+        }
+	}
+	 
+	public void checkXMLErrors(ActionEvent action){
 		 
-	 }
+	}
 	 
-	 public void checkXMLErrors(ActionEvent action){
+	public void addXMLFormat(ActionEvent action){
 		 
-	 }
+	}
 	 
-	 public void addXMLFormat(ActionEvent action){
+	public void minify(ActionEvent action){
 		 
-	 }
+	}
 	 
-	 public void minify(ActionEvent action){
+	public void convertJson(ActionEvent a){
 		 
-	 }
+	}
 	 
-	 public void convertJson(ActionEvent a){
+	public void compress(ActionEvent a) {
 		 
-	 }
+	}
 	 
-	 public void compress(ActionEvent a) {
+	public void decompress(ActionEvent a) {
 		 
-	 }
+	}
 	 
-	 public void decompress(ActionEvent a) {
-		 
-	 }
+	public void save(ActionEvent action){
 	 
-	 public void save(ActionEvent action){
-	 
-	 }	 
+	}	 
 }
