818fa60966388e94efd14f1b1c07da404c017ea2
islamzedd <islamalalamy@gmail.com>
2022-12-11 04:48:10 +0200

    added gui components for first phase

---
+++ src/xml/Controller.java
@@ -0,0 +1,44 @@
+package xml;
+
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+public class Controller {
+	private Stage stage;
+	
+	void init(Stage s) {
+        stage=s;
+    }
+	
+	 public void chooseXMLFile(ActionEvent actionevent){
+		 
+	 }
+	 
+	 public void checkXMLErrors(ActionEvent action){
+		 
+	 }
+	 
+	 public void addXMLFormat(ActionEvent action){
+		 
+	 }
+	 
+	 public void minify(ActionEvent action){
+		 
+	 }
+	 
+	 public void convertJson(ActionEvent a){
+		 
+	 }
+	 
+	 public void compress(ActionEvent a) {
+		 
+	 }
+	 
+	 public void decompress(ActionEvent a) {
+		 
+	 }
+	 
+	 public void save(ActionEvent action){
+	 
+	 }	 
+}
---
+++ src/xml/Main.java
@@ -0,0 +1,28 @@
+package xml;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	@Override
+    public void start(Stage stage) throws Exception {
+        
+        FXMLLoader fxmlLoader=new FXMLLoader(Main.class.getResource("Main.fxml"));
+        Parent root=fxmlLoader.load();
+        Controller control=(Controller) fxmlLoader.getController();
+        control.init(stage);
+        Scene s=new Scene(root);
+        stage.setScene(s);
+        stage.setTitle("XML ");
+        stage.show();
+    }
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
